e7e53b6ba5522e42024aa7c078552a307d1609d4
Dipanshu <220020020@iitdh.ac.in>
2025-09-09 11:56:03 +0530

    commit changes

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #define RED_LED (1<<1)
 #define BLUE_LED (1<<2)
 #define GREEN_LED (1<<3)
-#define SW1 (1<<0)
-#define SW2 (1<<4)
+#define SW2 (1<<0)
+#define SW1 (1<<4)
 
 #define ENABLE (1<<0)
 #define COUNT (1<<16)
@@ -31,17 +31,33 @@ int main(void)
     GPIO_PORTF_DATA_R = 0x00;
 
     //SETUP
-    STRELOAD = 0x7A1200; //clkfreq = 16 MHz
+    STRELOAD = 7999999; //clkfreq = 16 MHz
     STCTRL |= (1<<2);
     int x;
     while(1){
+
         STCTRL |= 1;
         x=0;
 
         GPIO_PORTF_DATA_R ^= RED_LED;
         //DELAY_LOOP
         while (x==0){
-            x = STCTRL & (1<<16); //CHECK IF COUNT == 1, IF YES THEN THE TIMER HAS CROSSED 0 THEN COME OUT OF LOOP
+            x = STCTRL & (1<<16);
+            //CHECK IF COUNT == 1, IF YES THEN THE TIMER HAS CROSSED 0 THEN COME OUT OF LOOP
+            if ((GPIO_PORTF_DATA_R & SW1 ) == 0)
+            {
+                GPIO_PORTF_DATA_R |= GREEN_LED;}
+            else
+            {
+                GPIO_PORTF_DATA_R &= ~GREEN_LED;
+            }
+
+
+            if ((GPIO_PORTF_DATA_R & SW2 ) == 0)
+            {
+                GPIO_PORTF_DATA_R |= BLUE_LED; }
+            else {
+                GPIO_PORTF_DATA_R &= ~BLUE_LED;}
         }
 
 
@@ -49,5 +65,8 @@ int main(void)
         STCTRL &= ~ENABLE;
         STCURRENT = 0x0;
         STCTRL |= ENABLE;
+
+        GPIO_PORTF_DATA_R &= ~GREEN_LED;
+        GPIO_PORTF_DATA_R &= ~BLUE_LED;
         }
 }
